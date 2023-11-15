3ad4e08e4c3a15372437d13ca1659340e3dd11fc
sc-DaniloSanna <danilo.sanna@smart-contact.it>
2023-11-15 09:07:33 +0000

    feat: barra di ricerca con filtro fatta

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,36 +1,42 @@
 <template>
-    <div class="card flex justify-content-center">
-        <MultiSelect v-model="selectedCities" :options="cities" filter optionLabel="name" placeholder="Select Cities"
-            :maxSelectedLabels="3" class="bg-slate-100 border-2 w-full md:w-20rem" />
+    <div class="relative">
+        <svg width="20" height="20" fill="currentColor"
+        class="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-blue-500"
+        aria-hidden="true">
+        <path fill-rule="evenodd" clip-rule="evenodd"
+            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+            class="" />
+    </svg>
+        <InputText v-model="value" placeholder="Search"
+            class="focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm"
+            type="text" aria-label="Filter projects" />
     </div>
-  <div v-for="product in products" :key="product.id">
-            <div class="capitalize font-bold">{{ product.name }}</div>
-            <div>Prezzo: {{ product.price }}</div>
-            <button @click="addToCart(product)" class="h-10 px-6 font-semibold rounded-md bg-emerald-500 hover:bg-emerald-600 text-white">Aggiungi al carrello</button>
-        </div>
+    
+    <div v-for="product in filteredProducts" :key="product.id">
+        <div class="capitalize font-bold">{{ product.name }}</div>
+        <div>Prezzo: {{ product.price }}</div>
+        <button @click="addToCart(product)"
+            class="h-10 px-6 font-semibold rounded-md bg-emerald-500 hover:bg-emerald-600 text-white">
+            Aggiungi al carrello
+        </button>
+    </div>
 </template>
 <script setup>
+import { products } from '../../db.json'
+import { useUserCartStore } from '../stores/userCart.store'
+import { computed, ref } from 'vue'
 
-import { products } from '../../db.json';
-import { useUserCartStore } from '../stores/userCart.store';
-import { ref } from "vue";
+const value = ref(null)
+const store = useUserCartStore()
 
-const selectedCities = ref();
-const cities = ref([
-    { name: 'New York', code: 'NY' },
-    { name: 'Rome', code: 'RM' },
-    { name: 'London', code: 'LDN' },
-    { name: 'Istanbul', code: 'IST' },
-    { name: 'Paris', code: 'PRS' }
-]);
+const filteredProducts = computed(() => {
+  const searchTerm = value.value ? value.value.toLowerCase() : ''
 
-const store = useUserCartStore();
-
+  return products.filter(product => product.name.toLowerCase().startsWith(searchTerm))
+})
 
 const addToCart = (product) => {
-    store.addToCart(product);
-};
-
+    store.addToCart(product)
+}
 </script>
-<style lang="scss">
-</style>
+<style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import PrimeVue from 'primevue/config';
 import App from './App.vue'
 import router from './router'
 import MultiSelect from 'primevue/multiselect';
+import InputText from 'primevue/inputtext';
 
 const app = createApp(App)
 
@@ -14,5 +15,5 @@ app.use(createPinia())
 app.use(router)
 
 app.use(PrimeVue);
-app.component('MultiSelect', MultiSelect);
+app.component('InputText', InputText);
 app.mount('#app')
