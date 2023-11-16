ce739d3629cf925081a35689d10af1791fe4e325
sc-DaniloSanna <danilo.sanna@smart-contact.it>
2023-11-16 10:35:09 +0000

    -

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Fake store" />
+      <HelloWorld msg="VueShop 3.0" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
